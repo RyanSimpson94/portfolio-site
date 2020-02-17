558f625f6fbcdafbd0ccbc61668048444d883f94
Ryan <RyanSimpson94@users.noreply.github.com>
2020-02-17 00:57:00 +0000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main id="app">
+    <header-component></header-component>
+    <nav class="navigation">
+      <button>CV</button>
+      <button>Contact</button>
+    </nav>
+  </main>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'App',
+    components: {
+      'header-component': () => import('./components/header')
+    }
+  }
+</script>
